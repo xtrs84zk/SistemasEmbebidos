dd16ac4954a315a4b993984c88da690288e850f9
xtrs84zk <xtrs84zk@gmail.com>
2020-10-08 20:27:50 -0500

    Add Práctica 2.

---
+++ src/Practica2.ino
@@ -0,0 +1,42 @@
+/*
+ * Project Práctica 2
+ * Description: Enciende y apaga cada led por medio segundo.
+ * Author: Javier Sánchez
+ * Date: Octubre 7, 2020.
+ */
+int led1 = D0;
+int led2 = D1;
+int led3 = D2;
+int led4 = D3;
+
+int tiempoS = 500; // medio segundo
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  digitalWrite(led1, HIGH); // primer led
+  delay(tiempoS);
+  digitalWrite(led1, LOW);
+  delay(tiempoS);
+  digitalWrite(led2, HIGH); // segundo led
+  delay(tiempoS);
+  digitalWrite(led2, LOW);
+  delay(tiempoS);
+  digitalWrite(led3, HIGH); // tercer led
+  delay(tiempoS);
+  digitalWrite(led3, LOW);
+  delay(tiempoS);
+  digitalWrite(led4, HIGH); // cuarto led
+  delay(tiempoS);
+  digitalWrite(led4, LOW);
+  delay(tiempoS);
+}
