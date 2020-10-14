c58bc8e580c942fa4c918407e5717574b85fde38
xtrs84zk <xtrs84zk@gmail.com>
2020-10-14 18:45:15 -0500

    Update Practica 4, Practica 5 Utilizando ciclos para asignación y manipulación de LEDs.

--- src/Practica4.ino
+++ src/Practica4.ino
@@ -5,49 +5,35 @@
  * Author: Javier Sánchez
  * Date: Octubre 8, 2020.
  */
-int led1 = D0;
-int led2 = D1;
-int led3 = D2;
-int led4 = D3;
 
-int tiempoS = 500; // medio segundo
+int tiempo = 500; // medio segundo
 
 // setup() runs once, when the device is first turned on.
-void setup() {
+void setup()
+{
   // Put initialization like pinMode and begin functions here.
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
+  for (int i = 0; i <= 3; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
-  //encender hacia un lado
-  digitalWrite(led1, HIGH); // primer led
-  delay(tiempoS);
-  digitalWrite(led1, LOW);
-  delay(tiempoS);
-  digitalWrite(led2, HIGH); // segundo led
-  delay(tiempoS);
-  digitalWrite(led2, LOW);
-  delay(tiempoS);
-  digitalWrite(led3, HIGH); // tercer led
-  delay(tiempoS);
-  digitalWrite(led3, LOW);
-  delay(tiempoS);
-  digitalWrite(led4, HIGH); // cuarto led
-  delay(tiempoS);
-  //encender hacia el otro
-  digitalWrite(led4, LOW);
-  delay(tiempoS);
-  digitalWrite(led3, HIGH); // segundo led
-  delay(tiempoS);
-  digitalWrite(led3, LOW);
-  delay(tiempoS);
-  digitalWrite(led2, HIGH); // tercer led
-  delay(tiempoS);
-  digitalWrite(led2, LOW);
-  delay(tiempoS);
+void loop()
+{
+  delay(tiempo);
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(tiempo);
+    digitalWrite(i, LOW);
+    delay(tiempo);
+  }
+  for (int i = 3; i > 0; i--)
+  {
+    digitalWrite(i, HIGH);
+    delay(tiempo);
+    digitalWrite(i, LOW);
+    delay(tiempo);
+  }
 }
--- src/Practica5.ino
+++ src/Practica5.ino
@@ -4,41 +4,33 @@
  * Author: Javier Sánchez
  * Date: Octubre 8, 2020.
  */
-int led1 = D0;
-int led2 = D1;
-int led3 = D2;
-int led4 = D3;
 
-int tiempoS = 500; // medio segundo
+int tiempo = 500; // medio segundo
 
 // setup() runs once, when the device is first turned on.
-void setup() {
+void setup()
+{
   // Put initialization like pinMode and begin functions here.
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
+  for (int i = 0; i <= 3; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop() {
+void loop()
+{
   // The core of your code will likely live here.
   //encendiendo leds
-  digitalWrite(led1, HIGH); // primer led
-  delay(tiempoS);
-  digitalWrite(led2, HIGH); // segundo led
-  delay(tiempoS);
-  digitalWrite(led3, HIGH); // tercer led
-  delay(tiempoS);
-  digitalWrite(led4, HIGH); // cuarto led
-  delay(tiempoS);
+  for (int i = 0; i < 4; i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(tiempo);
+  }
   //apagando leds
-  digitalWrite(led4, LOW); // cuarto led
-  delay(tiempoS);
-  digitalWrite(led3, LOW); // tercer led
-  delay(tiempoS);
-  digitalWrite(led2, LOW); // segundo led
-  delay(tiempoS);
-  digitalWrite(led1,LOW); //primer led
-  delay(tiempoS);
+  for (int i = 3; i >= 0; i--)
+  {
+    digitalWrite(i, LOW);
+    delay(tiempo);
+  }
 }
