2fbe6b82e43e5f432e5614c59ebb7bb0fca92ba1
xtrs84zk <xtrs84zk@gmail.com>
2020-12-27 22:58:02 -0600

    Add Práctica 30.

---
+++ src/Practica30/Practica30.ino
@@ -0,0 +1,53 @@
+Servo servo;
+int minute = 0;
+int hour = 0;
+int day = 0;
+int month = 0;
+bool hasBeenFeed = false;
+void setup()
+{
+    pinMode(D0, OUTPUT);
+    servo.attach(D0);
+    pinMode(D1, INPUT_PULLUP);
+    Time.zone(-6);
+    Particle.variable("minute", &minute, INT);
+    Particle.variable("hour", &hour, INT);
+}
+
+void loop()
+{
+    minute = Time.minute();
+    hour = Time.hour();
+    day = Time.day();
+    month = Time.month();
+    if (digitalRead(D1) == HIGH)
+    {
+        alimentarPez();
+    }
+    if (month == 12 && day == 27 && hour == 22 && minute == 46)
+    {
+        if (!hasBeenFeed)
+        {
+            alimentarPez();
+            hasBeenFeed = true;
+        }
+    }
+    if (month == 12 && day == 27 && hour == 22 && minute == 47)
+    {
+        hasBeenFeed = false;
+    }
+}
+
+void alimentarPez()
+{
+    for (int i = 1; i < 255; i++)
+    {
+        servo.write(i);
+        delay(15);
+    }
+    for (int i = 255; i > 1; i--)
+    {
+        servo.write(i);
+        delay(15);
+    }
+}
