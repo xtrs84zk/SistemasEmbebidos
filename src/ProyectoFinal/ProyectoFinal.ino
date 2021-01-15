23c1140f37d79f837508abf03f2d60378156716a
xtrs84zk <xtrs84zk@gmail.com>
2021-01-15 03:07:20 -0600

    v2

--- src/ProyectoFinal/ProyectoFinal.ino
+++ src/ProyectoFinal/ProyectoFinal.ino
@@ -1,21 +1,40 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <Adafruit_DHT_Particle.h>
+
+// This #include statement was automatically added by the Particle IDE.
+#include <HC-SR04.h>
+#include "lib1.h"
+
+#define DHTPIN D0
+#define DHTTYPE DHT11
+#define TRIGGERPIN A0
+#define ECHOPIN D1
+DHT dht(DHTPIN, DHTTYPE);
 // interfaz gráfica : https://github.com/xtrs84zk/coffeeboard
-Servo servo;    // Objeto donde se asigna el servomotor
+Servo servo; // Objeto donde se asigna el servomotor
+HC_SR04 rangefinder = HC_SR04(TRIGGERPIN, ECHOPIN);
+double distanceInCentimeters = 0;
 int minute = 0; // Variable para almacenar el minuto actual
 int hour = 0;   // Variable para almacenar la hora actual
-int lastActivationMinute = 0;
-int lectura = 0;
-double voltaje = 0.0;
-double gradosCelsius = 0.0;
+String lastActivation = "0:00am";
 String nextActivation = "9:40am";
+int startedBoiling = 0;
 double coffeetemp = 0;
+double temp = 0;
 bool hasBeenBoiled = false; // Bandera para almacenar si el café ha sido calentado recientemente
+bool triedToStartButCant = false;
+bool elCafeEstaListo = false;
 void setup()
 {
+    rangefinder.init();
+    dht.begin();
     pinMode(A1, INPUT);
-    Particle.variable("minute", &minute, INT);                    // Se comparte la variable minute con la nube de particle
-    Particle.variable("hour", &hour, INT);                        // Se comparte la variable hour con la nube de particle
     Particle.variable("nextActivation", &nextActivation, STRING); // Se comparte la variable nextActivation con la nube de particle
+    Particle.variable("lastActivation", &lastActivation, STRING); // Se comparte la variable nextActivation con la nube de particle
     Particle.variable("coffeetemp", &coffeetemp, DOUBLE);         // Se comparte la variable coffeetemp con la nube de particle
+    Particle.variable("distanceInCentimeters", &distanceInCentimeters, DOUBLE);         // Se comparte la variable coffeetemp con la nube de particle
+    Particle.variable("triedToStartButCant", &triedToStartButCant, BOOLEAN);         // Se comparte la variable coffeetemp con la nube de particle
+    Particle.variable("elCafeEstaListo", &elCafeEstaListo, BOOLEAN);         // Se comparte la variable coffeetemp con la nube de particle
     Time.zone(-6);                                                // Se establece la zona horaria en corcondancia con aquella de México actualmente, GMT -6
     pinMode(D3, OUTPUT);
     pinMode(D7, OUTPUT);
@@ -24,17 +43,18 @@ void setup()
 
 void loop()
 {
-    minute = Time.minute();   // Se asigna el minuto actual a la variable minute
-    hour = Time.hour();       // Se asigna la hora actual a la variable hora
-    lectura = analogRead(A1); //0-4095
-    voltaje = ((lectura * 3.3) / 4095.0);
-    gradosCelsius = ((3.3 * (10.0 / voltaje)) - 10); // Se lee la temperatura del café
-    coffeetemp = gradosCelsius;
+    if(elCafeEstaListo) {stopBoilling()}
+    minute = Time.minute(); // Se asigna el minuto actual a la variable minute
+    hour = Time.hour();     // Se asigna la hora actual a la variable hora
+    temp = dht.getTempCelcius();
+    if(!std::isnan(temp)){
+        coffeetemp = temp;    
+    }
+    distanceInCentimeters = rangefinder.distInch() * 2.54;
     if (hour == 9 && minute == 40) // Se compara la hora actual con las 19:24
     {
         if (!hasBeenBoiled) // Se verifica que el pez no haya sido alimentado en este minuto
         {
-            lastActivationMinute = 40;
             actuallyBoil();
             hasBeenBoiled = false; // Se establece que el pez ha sido almentado
         }
@@ -44,29 +64,40 @@ void loop()
         hasBeenBoiled = true; // Se establece que el pez no ha sido alimentado para la próxima iteración
         stopBoilling();
     }
+    if(triedToStartButCant){
+
+    }
 }
 
 // Función que enciende la cafetera
-void boilCoffe(String par)
+int boilCoffe(String par)
 {
     if (par == "on")
     {
         actuallyBoil();
+        return 0;
     }
     else if (par == "off")
     {
         stopBoilling();
+        return 0;
     }
 }
 
 void actuallyBoil()
 {
-    digitalWrite(D7, LOW);
-    digitalWrite(D3, LOW);
+    if(distanceInCentimeters > 5){
+        elCafeEstaListo = false;
+        lastActivation = minute + ":"+  hour;
+        digitalWrite(D7, HIGH);
+        digitalWrite(D3, LOW);
+    }else {
+        triedToStartButCant = true;
+    }
 }
 
 void stopBoilling()
 {
-    digitalWrite(D7, HIGH);
+    digitalWrite(D7, LOW);
     digitalWrite(D3, HIGH);
 }
